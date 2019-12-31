917f3b3c070b5f46a15e78c5863bc75643612e75
Abdelhalem Kassara <abdelhalemkassara@yahoo.com>
2019-12-31 12:11:19 -0500

    started generating meshes using code

--- 3d Racing Game/Assets/Scripts/MeshGenerator.cs
+++ 3d Racing Game/Assets/Scripts/MeshGenerator.cs
@@ -8,6 +8,8 @@ public class MeshGenerator : MonoBehaviour
     Mesh mesh;
     Vector3[] vertices;
     int[] triangles;
+    public int xSize = 20;
+    public int zSize = 20;
 
     void Start()
     {
@@ -16,23 +18,50 @@ public class MeshGenerator : MonoBehaviour
 
         CreateShape();
         UpdateMesh();
+
     }
 
+    private void Update()
+    {
+    }
     void CreateShape()
     {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
 
-        vertices = new Vector3[]{
-
-            new Vector3 (0,0,0),
-            new Vector3 (0,0,1),
-            new Vector3 (1,0,0),
-            new Vector3 (1,0,1)
-        };
-        triangles = new int[]
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
-            0,1,2,
-            1,3,2
-        };
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+
+            }
+
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+            }
+            vert++;
+        }
 
     }
     void UpdateMesh()
@@ -44,4 +73,5 @@ public class MeshGenerator : MonoBehaviour
 
     }
 
+   
 }
