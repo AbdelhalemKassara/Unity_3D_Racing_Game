b248215db3f672e84cd5d687ee89f144c1d3d801
Abdelhalem Kassara <abdelhalemkassara@yahoo.com>
2020-01-02 23:50:10 -0500

    added the buttons

---
+++ 3d Racing Game/Assets/Scripts/Buttons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void TrackList()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void PickTrack1()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void PickProceduralTrack()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void CarSelect()
+    {
+        SceneManager.LoadScene(4);
+
+    }
+
+}
